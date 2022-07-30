d4259ceca54d550a9b6cfe4562a873ffd704cf5e
MichalMaciejKopec <michalkopec8@gmail.com>
2022-07-30 08:59:15 +0200

    routing

--- oddam/src/App.js
+++ oddam/src/App.js
@@ -1,22 +1,23 @@
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+
+import Main from "./components/Main";
+import Login from "./components/Login";
+import Registration from "./components/Registration";
+import Logout from "./components/Logout";
+import Form from "./components/Form";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Main/>}/>
+                <Route path="login" element={<Login/>}/>
+                <Route path="registration" element={<Registration/>}/>
+                <Route path="logout" element={<Logout/>}/>
+                <Route path="oddam" element={<Form/>}/>
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
---
+++ oddam/src/components/Main.js
@@ -0,0 +1,12 @@
+
+
+const Main = ()=>{
+
+    return(
+        <div>
+            <p>Main</p>
+        </div>
+    )
+}
+
+export default Main;
